61ae94fc8677d23a81b183dd575bf074df3bc75c
radhakrishnan S <krish.selvaraj@gmail.com>
2017-07-07 19:01:49 +0530

    The border is added

--- CalendarTasks/CalendarViewController.swift
+++ CalendarTasks/CalendarViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CalendarViewController: UIViewController {
     var dataSource : CalendarDataSource!
+    
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
--- CalendarTasks/DayCell.swift
+++ CalendarTasks/DayCell.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class DayCell: UICollectionViewCell {
     
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var monthLabel: UILabel!
+    var borderView : UIView!
+    override func awakeFromNib() {
+        var frame = self.bounds
+        frame.origin.y = self.bounds.height - 1
+        frame.size.height = 1
+        frame.size.width = self.contentView.frame.size.width
+        borderView = UIView(frame: frame)
+        borderView.backgroundColor = UIColor.lightGray
+        self.contentView.addSubview(borderView)
+        self.backgroundColor = UIColor.red
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+            borderView.frame =  CGRect(x: 0, y: 0, width: self.frame.size.width, height: 1)
+    }
 }

50c91c8b72af4b3b74103eaa72bb40e61c9b45a8
radhakrishnan S <krish.selvaraj@gmail.com>
2017-07-13 22:16:09 +0530

    Section title rendered

--- CalendarTasks/TasksTableViewController.swift
+++ CalendarTasks/TasksTableViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+let sectionHeaderReuseIdentifier = "DaySectionHeader"
 class TasksTableViewController: UITableViewController {
-
+    
+    var currentYear : Year?
+    let dateFormatter = DateFormatter()
+    var currentMonth : Month? = nil
+    var dayCountUntilCurrentMonth = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let nib = UINib(nibName: "DayHeaderView", bundle: nil)
+        self.tableView.register(nib, forHeaderFooterViewReuseIdentifier: sectionHeaderReuseIdentifier)
+        self.tableView.reloadData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,23 +36,59 @@ class TasksTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return currentYear == nil ? 0 : currentYear!.totalDays
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
-
-    /*
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if let year = currentYear {
+            if currentMonth == nil{
+                currentMonth = year.monthList.first
+                dayCountUntilCurrentMonth = (currentMonth?.totalDays)!
+            }else if section == dayCountUntilCurrentMonth{
+                currentMonth = year.nextMonth(currentMonth: currentMonth!)
+                if let month = currentMonth  {
+                    dayCountUntilCurrentMonth = dayCountUntilCurrentMonth + month.totalDays
+                }
+                
+            }else{
+                
+            }
+            let totalDays = (currentMonth?.totalDays)!
+            let dayIndex = section - dayCountUntilCurrentMonth + totalDays
+            
+            let weekDaysTitle = dateFormatter.weekdaySymbols[ (dayIndex + (currentMonth?.firstDay)!) % 7 ]
+            let monthName = dateFormatter.monthSymbols[(currentMonth?.month)!-1]
+            if let day  = currentMonth?.dayList[dayIndex]{
+                let title = weekDaysTitle + " " + String(describing: day.day) + " " + monthName
+                return title
+            }
+            else{
+                return nil
+            }
+           
+            
+            
+        }else{
+            return nil
+        }
+    }
+    
+    public override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat{
+        return 20
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        
+        
+        //let day =
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
